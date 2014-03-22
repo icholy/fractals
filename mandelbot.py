9f6f0798fbf348b627ca39ed5480ffcf9cd8ca9d
Ilia Choly <ilia.choly@gmail.com>
2014-03-22 00:26:20 -0400

    Update mandelbot.py

--- mandelbot.py
+++ mandelbot.py
@@ -1,8 +1,8 @@
+import Image
+import os
 
-import itertools
 
-
-def escaped(x, y):
+def has_escaped(x, y):
 	"has the point escaped a circle of radius 2"
 	return x*x + y*y > 4
 
@@ -18,23 +18,29 @@ def iterate(x, y, limit=10):
 	it_count = 0
 	for x, y in it:
 		it_count += 1
-		if escaped(x, y):
+		if has_escaped(x, y):
 			return it_count
 		if it_count > limit:
-			return None
+			return -1
+
+def get_colour(index):
+	index = (index % 3) - 1
+	return [(255, 0, 0), (0, 255, 0), (0, 0, 255)][index]
 
 def main():
-	colors = itertools.cycle(["red", "yellow", "blue"])
-	size = 100
+	img = Image.new('RGB', (1000, 1000), "black")
+	pixels = img.load()
+	size = 1000
 	offset = size / 2
 	for i in xrange(size):
 		for j in xrange(size):
-			x, y = i - offset, j - offset
+			x = i - offset
+			y = j - offset
 			it_count = iterate(x, y)
-			if it_count is not None:
-				color = itertools.isslice(colors, it_count, it_count+1)
-				draw(x, y, color)
-
+			if it_count != -1:
+				pixels[i, j] = get_colour(it_count)
+	img.save('out.bmp')
+	os.startfile('out.bmp')
 
 if __name__ == '__main__':
 	main()
